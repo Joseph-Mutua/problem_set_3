a0858ac038241f407806e38f5dd18321b8d9a4fd
Joseph Mutua <Mutuaj793@gmail.com>
2022-01-05 12:18:53 +0300

    Create aweb crawler function and function to clear symbols off retruned text

--- crawler.py
+++ crawler.py
@@ -7,3 +7,50 @@ import requests
 from bs4 import BeautifulSoup
 import operator
 from collections import Counter
+
+# Function defining the web-crawler which will fetch info 
+# from a given website and push the resulting contents to the
+# second function clear_symbols()
+
+def crawl(url)
+    # empty the wordlist variable to store the fetched contents
+    wordlist = []
+    source_code = requests.get(url).text
+    
+    # Create the beautifulSoup object to ping the requested url for data
+    soup = BeautifulSoup(source_code, 'html.parser')
+    
+    # Store resulting text in <div> tags with class <entry-content>
+    for each_text in soup.findAll('div', {'class': 'entry-content'}):
+        content = each_text.text
+ 
+        # use split() to break the sentence into
+        # words and convert them into lowercase
+        words = content.lower().split()
+ 
+        for each_word in words:
+            wordlist.append(each_word)
+        clear_symbols(wordlist)
+        
+        
+# Function to remove all unwanted symbols from the wordlist
+
+def clear_symbols(wordlist):
+    clean_list = []
+    for word in wordlist:
+        symbols = "!@#$%^&*()_-+={[}]|\;:\"<>?/., "
+        
+        for i in range(len(symbols)):
+            word = word.replace(symbols[i], '')
+            
+        if len(word) > 0:
+            clean_list.append(word)
+            
+    create_dictionary(clean_list)
+    
+    
+# Function to create dictionary containing each words count
+
+        
+
+
