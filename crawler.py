fbda5eeb003f66638d8d13e955313eb517a9cd7d
Joseph Mutua <Mutuaj793@gmail.com>
2022-01-05 11:55:54 +0300

    inital commit

---
+++ crawler.py
@@ -0,0 +1,9 @@
+# request module to send HTTP/1.1 requests
+# beautifulsoup4 module for parsing HTML/XMLto extract data
+# operator module for operator functions
+# collections module to implement high-performance container datatypes
+
+import requests
+from bs4 import BeautifulSoup
+import operator
+from collections import Counter
