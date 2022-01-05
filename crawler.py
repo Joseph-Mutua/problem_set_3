3cdb12433816e28c046c6db6342dd4b35461f8a6
Joseph Mutua <Mutuaj793@gmail.com>
2022-01-05 14:40:47 +0300

    get the words and  count from the crawled webpage

--- crawler.py
+++ crawler.py
@@ -5,52 +5,73 @@
 
 import requests
 from bs4 import BeautifulSoup
-import operator
 from collections import Counter
 
-# Function defining the web-crawler which will fetch info 
+# Function defining the web-crawler which will fetch info
 # from a given website and push the resulting contents to the
 # second function clear_symbols()
 
-def crawl(url)
+
+def crawl(url):
     # empty the wordlist variable to store the fetched contents
     wordlist = []
     source_code = requests.get(url).text
-    
+
     # Create the beautifulSoup object to ping the requested url for data
     soup = BeautifulSoup(source_code, 'html.parser')
-    
-    # Store resulting text in <div> tags with class <entry-content>
-    for each_text in soup.findAll('div', {'class': 'entry-content'}):
+
+    # Store resulting text in <div> tags
+    for each_text in soup.findAll('div'):
         content = each_text.text
- 
+
         # use split() to break the sentence into
         # words and convert them into lowercase
         words = content.lower().split()
- 
+
         for each_word in words:
             wordlist.append(each_word)
         clear_symbols(wordlist)
-        
-        
-# Function to remove all unwanted symbols from the wordlist
 
+
+# Function to remove all unwanted symbols from the wordlist
 def clear_symbols(wordlist):
     clean_list = []
     for word in wordlist:
         symbols = "!@#$%^&*()_-+={[}]|\;:\"<>?/., "
-        
+
         for i in range(len(symbols)):
             word = word.replace(symbols[i], '')
-            
+
         if len(word) > 0:
             clean_list.append(word)
-            
+
     create_dictionary(clean_list)
-    
-    
+
+
 # Function to create dictionary containing each words count
+def create_dictionary(clean_list):
+    word_count = {}
 
-        
+    for word in clean_list:
+        if word in word_count:
+            word_count[word] += 1
+        else:
+            word_count[word] = 1
 
 
+#  Get the count for each word in the crawled page stored
+# in the dictionary in key/value pairs
+# Use Counter from collections which converts the elements to
+# keys and values will be the count of the elements
+ 
+ 
+    c = Counter(word_count)
+ 
+    # Print the word:count key/value pairs to console
+    print(c)
+
+# Driver code
+if __name__ == '__main__':
+    url = "https://www.geeksforgeeks.org/programming-language-choose/"
+    # starts crawling and prints output
+    crawl(url)
